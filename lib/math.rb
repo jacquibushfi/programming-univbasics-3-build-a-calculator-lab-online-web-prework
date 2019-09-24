e81d5c0d3754c70de73dcb44b7e7646dc4b18aa7
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-24 17:54:54 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,21 @@
 def addition(num1, num2)
-
+  a =  num1 + num2
+  puts a
 end
 
 def subtraction(num1, num2)
-
+  s = num1 - num2
+  put s
 end
 
 def division(num1, num2)
-
+  d = num1 / num2
+  put d
 end
 
 def multiplication(num1, num2)
-
+  m = num1 * num2
+  put m
 end
 
 def modulo(num1, num2)
