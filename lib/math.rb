81d719aed958c28c2d2685f274b0e7b9dd06103e
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-24 18:13:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -12,7 +12,7 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  $d = num1 / num2
+  d = num1 / num2
   p d
 end
 
@@ -25,8 +25,8 @@ def modulo(num1, num2)
   mod = num1 % num2
   p mod
 end
-num = $d
-def square_root(num)
+
+def square_root(num = 16)
   sq = num.sqrt()
   p sq
 end
