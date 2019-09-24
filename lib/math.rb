046220a7c383dae2924e760048236ed237227804
Jacqui Bush <jacqui@stainedglassofmckinney.com>
2019-09-24 18:20:14 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,33 @@
-def addition(num1, num2)
+num1 =5
+num2 = 4
 
+def addition(num1, num2)
+  a =  num1 + num2
+  p a
 end
 
 def subtraction(num1, num2)
-
+  s = num1 - num2
+  p s
 end
 
 def division(num1, num2)
-
+  d = num1 / num2
+  p d
 end
 
 def multiplication(num1, num2)
-
+  m = num1 * num2
+  p m
 end
 
 def modulo(num1, num2)
-
+  mod = num1 % num2
+  p mod
 end
 
-def square_root(num)
-
+def square_root(num = 81)
+ require 'cmath' 
+ x = CMath.sqrt(num) 
+ p x 
 end
